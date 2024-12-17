cf4060ce95bf5bc41689e8ae25897d0d4291458f
Pablo Jiménez <pablo.jimeneza@hotmail.com>
2024-12-16 19:50:21 -0600

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
-  server: {
-    https: true,
-    cors: true,
-  },
+  plugins: [react()],
+  server: {},
 })
