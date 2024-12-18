b2d0ec36e53bbe9c4a69321794598bf4bed62036
Pablo Jiménez <pablo.jimeneza@hotmail.com>
2024-12-18 15:57:15 -0600

    (App): Test

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,4 +1,4 @@
 import axios from "./axios";
 
 export const register = (user) => axios.post("/results", user);
-export const results = (sandbox, event) => axios.get(`/results/${0}/event/${event}`);
+export const results = (sandbox, event) => axios.get(`/results/${sandbox}/event/${event}`);
